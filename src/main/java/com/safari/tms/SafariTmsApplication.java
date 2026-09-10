f318b30c605a1d650502464e8c769465cd76951b
it25103191 <it25103191@my.sliit.lk>
2026-09-10 12:09:26 +0530

    Initial project setup

---
+++ src/main/java/com/safari/tms/SafariTmsApplication.java
@@ -0,0 +1,13 @@
+package com.safari.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafariTmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafariTmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safari/tms/SafariTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safari.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafariTmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
