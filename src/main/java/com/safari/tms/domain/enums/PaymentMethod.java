2506353bfa18ba76b8c2769f13bba2ff00755266
it25103191 <it25103191@my.sliit.lk>
2026-09-14 13:55:47 +0530

    Add PaymentStatus enum, PaymentMethod enum, RefundStatus enum and Payment and Refund entities

---
+++ src/main/java/com/safari/tms/domain/enums/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.safari.tms.domain.enums;
+
+public enum PaymentMethod {
+    CREDIT_CARD, DEBIT_CARD, BANK_TRANSFER, MOBILE_MONEY, CASH
+}
---
+++ src/main/java/com/safari/tms/domain/enums/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.safari.tms.domain.enums;
+
+public enum PaymentStatus {
+    PENDING, SUCCESS, DECLINED, TIMEOUT, REFUNDED
+}
